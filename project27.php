c478ccc2c77fbffb722cd5136bfa55e9360b801f
Gipson Suu <91767873+gipsonsuu@users.noreply.github.com>
2022-04-23 10:51:14 +0900

    add new project

---
+++ project27.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+    $_COOKIE
+    $_SESSION
+    */
+    setcookie("name", "Daniel", time() - 86400);
+
+    $_SESSION['name'] = "12";
+
+    ?>
+</body>
+</html>
